a699b5d15482073cd5c4325d3b29b06298a38933
rosedimple <rosedimplegeorge@gmail.com>
2018-04-13 16:20:09 -0400

    CREATE & READ Works for the User End to End

--- routes/users.js
+++ routes/users.js
@@ -15,6 +15,30 @@ router.get('/', function(req, res, next) {
     console.log(error)
   })
 })
+
+//CREATE A NEW USER
+
+router.get('/new', (req, res) => {
+  res.render('users/new')
+})
+
+router.post('/',(req, res) => {
+  const user = new User({
+    userName: req.body.userName,
+    email: req.body.email,
+    yrs_Of_Exp: req.body.yrs_Of_Exp
+  })
+
+  user.save()
+  .then((savedUser) => {
+    res.redirect(`/users/${savedUser._id}`)
+  }).catch((error) => {
+    console.log(error)
+  })
+
+})
+
+
 // GET TechStack Index. 
 router.get('/:id', (req, res) => {
   User.findById(req.params.id)
@@ -25,23 +49,6 @@ router.get('/:id', (req, res) => {
   })
   .catch((error) => {
       console.log(error)
-  })
-})
-
-//CREATE A NEW USER
-
-router.post('/',(req, res) => {
-  var user = new User({
-    userName: req.body.userName,
-    email: req.body.email,
-    yrs_Of_Exp: req.body.yrs_Of_Exp
-  })
-  user.save()
-  .then((savedUser) => {
-    console.log(user)
-    res.send(user)
-  }).catch((error) => {
-    console.log(error)
   })
 })
 
